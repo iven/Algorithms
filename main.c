81e8ee8eda82e954693b111d09f5b6178a48b153
iven <ivenvd@gmail.com>
2014-02-19 02:11:30 +0800

    Add YCM configuration file.

--- main.c
+++ main.c
@@ -10,13 +10,13 @@ int main(void)
   int array[] = {5, 2, 4, 6, 1, 9, 7, 8, 3, 0};
   int length = GET_LENGTH(array);
 
-  //insertion_sort(array, length);
+  insertion_sort(array, length);
   //shell_sort(array, length);
   //heap_sort(array, length);
   //merge_sort(array, length);
-  quick_sort(array, length);
+  //quick_sort(array, length);
 
-  printf("Sorted Heap:\n");
+  printf("Sorted Array:\n");
   print_array(array, GET_LENGTH(array));
 
   return EXIT_SUCCESS;
